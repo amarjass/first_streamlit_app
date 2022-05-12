a6ba7c7ff2146e4e12c53c84ad2c45f00b3fa7f7
amarjass <105423167+amarjass@users.noreply.github.com>
2022-05-12 18:34:51 +0530

    Update streamlip_app.py
    Added  my_fruit_list = my_fruit_list.set_index('Fruit') to show fruits name in the index

--- streamlip_app.py
+++ streamlip_app.py
@@ -8,10 +8,14 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 import pandas
 my_fruit_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index('Fruit')
+#streamlit.dataframe(my_fruit_list)
+
+
 
 # lets put a pick list here so they can pick the fruits they want to include
 streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
 
 #display the table on the page 
 streamlit.dataframe(my_fruit_list)
+
