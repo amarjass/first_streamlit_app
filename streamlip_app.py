1c8e8ec687d2725dbbe3d21fb3e59c1181eb6737
amarjass <105423167+amarjass@users.noreply.github.com>
2022-05-12 18:59:48 +0530

    Update streamlip_app.py
    give the limited fruits for the selection

--- streamlip_app.py
+++ streamlip_app.py
@@ -14,7 +14,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
 # lets put a pick list here so they can pick the fruits they want to include
-streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
 
 #display the table on the page 
 streamlit.dataframe(my_fruit_list)
