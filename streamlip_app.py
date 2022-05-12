878a3f675359dbb38629f1d766efe0ae2a3f4950
amarjass <105423167+amarjass@users.noreply.github.com>
2022-05-12 17:45:02 +0530

    Update streamlip_app.py
    updated breakfast menu

--- streamlip_app.py
+++ streamlip_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title("My parent now healthy Diner")
-streamlit.header("Breakfast Menu")
-streamlit.text("OatMeal")
-streamlit.text("Smoothi")
-streamlit.text("egg")
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+
