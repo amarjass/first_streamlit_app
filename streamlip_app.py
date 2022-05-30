cd83977057a8670f7258ee8484801fe5f3b85b8f
amarjass <105423167+amarjass@users.noreply.github.com>
2022-05-30 12:14:56 +0530

    Update streamlip_app.py
    Comment the watermelon site and request for kiwi
    
    # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")

--- streamlip_app.py
+++ streamlip_app.py
@@ -26,7 +26,10 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit  Advice!')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+
 #streamlit.text(fruityvice_response.json())  # just writes the data on the screen
 
 
