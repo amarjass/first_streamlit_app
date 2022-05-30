909e490e834bcf5385dc5030f213cfbeb599b6dc
amarjass <105423167+amarjass@users.noreply.github.com>
2022-05-30 12:10:16 +0530

    Update streamlip_app.py
    Comment the text line from current code
    
    streamlit.text(fruityvice_response.json())  # just writes the data on the screen

--- streamlip_app.py
+++ streamlip_app.py
@@ -27,7 +27,7 @@ streamlit.header('Fruityvice Fruit  Advice!')
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())  # just writes the data on the screen
 
 
 #take the json version of the response  and normalized it.
