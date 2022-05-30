5c314242c92e6884c334e728221a624bfe878dde
amarjass <105423167+amarjass@users.noreply.github.com>
2022-05-30 12:25:30 +0530

    Update streamlip_app.py
    Write a code to take the input from user
    
    fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
    streamlit.write('The user entered ', Fruit_choice)
    
    comment request.get and add new url with fruit_choice
    #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)

--- streamlip_app.py
+++ streamlip_app.py
@@ -24,11 +24,14 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display Fruityvice api response
 streamlit.header('Fruityvice Fruit  Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered ', Fruit_choice)
 
 import requests
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #streamlit.text(fruityvice_response.json())  # just writes the data on the screen
 
