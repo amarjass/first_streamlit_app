4d92243462113bd87d026d5fb264f07fcf4cdfd0
amarjass <105423167+amarjass@users.noreply.github.com>
2022-05-31 20:46:02 +0530

    Update streamlip_app.py
    #Second  text box
    add_my_fruit = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
    streamlit.write('The user entered ', add_my_fruit)

--- streamlip_app.py
+++ streamlip_app.py
@@ -23,11 +23,17 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 #streamlit.dataframe(my_fruit_list)
 streamlit.dataframe(fruits_to_show)
 
-#New Section to display Fruityvice api response
+#New Section to display Fruityvice api response 
+#first text box
 streamlit.header('Fruityvice Fruit  Advice!')
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+
+#Second  text box
+add_my_fruit = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered ', add_my_fruit)
+
 
 import requests
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
