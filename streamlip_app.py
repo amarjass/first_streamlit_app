174840f131da081375e218c78f20622d2a8d677e
amarjass <105423167+amarjass@users.noreply.github.com>
2022-06-01 17:18:32 +0530

    Update streamlip_app.py

--- streamlip_app.py
+++ streamlip_app.py
@@ -30,20 +30,20 @@ streamlit.dataframe(fruits_to_show)
 ############################# Using try and except block
 #New Section to display Fruityvice api response 
 #first text box
-streamlit.header('Fruityvice Fruit  Advice!')
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-      streamlit.error("Please select a fruit to get information")
-  else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      #take the json version of the response  and normalized it.
-      fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
-      #output it to the screen as a table
-      streamlit.dataframe(fruityvice_normalized)
+#streamlit.header('Fruityvice Fruit  Advice!')
+#try:
+# fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#  if not fruit_choice:
+#      streamlit.error("Please select a fruit to get information")
+#  else:
+#      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#      #take the json version of the response  and normalized it.
+#      fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
+#      #output it to the screen as a table
+#      streamlit.dataframe(fruityvice_normalized)
     
-except URLError as e:
-  streamlit.error()
+#except URLError as e:
+#  streamlit.error()
   
   ############################# Using try and except block end
   
