ec8b238d54de04ea432de33ab7f8ff0ffc9c97b6
amarjass <105423167+amarjass@users.noreply.github.com>
2022-06-01 18:42:18 +0530

    Update streamlip_app.py
    ##Function block started
    #create a repeatable code block
    def get_fruitvice_data(this_fruit_choice):
        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
        #take the json version of the response  and normalized it.
        fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
        return fruityvice_normalized

--- streamlip_app.py
+++ streamlip_app.py
@@ -68,8 +68,26 @@ try:
 except URLError as e:      
    streamlit.error() 
 
-      
-      
+ ############### Function end
+    
+streamlit.header("the fruit load list contains")
+    ################### snowflake-related function   
+ def get_fruit_load_list():
+     with my_cnx.cursor() as my_cur
+          my_cur.execute("select * from fruit_load_list")
+          return my_cur.fetchall()
+ # add a button to load the fruits
+ if streamlit.button('Get Fruit load list'):
+    my_cur = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_list()
+    streamlit.dataframe(my_data_rows)
+    
+ ###############Snowflake function end###########
+    
+    
+    
+    
+    
         
 # streamlit.write('The user entered ', fruit_choice)
 
@@ -91,18 +109,21 @@ except URLError as e:
 streamlit.stop()
 
 # import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
+#my_cur.execute("SELECT * from fruit_load_list")
+#my_data_rows = my_cur.fetchall()
 #my_data_row = my_cur.fetchone()
-#streamlit.text("the fruit load list contains")
+##streamlit.text("the fruit load list contains")
+
 streamlit.header("the fruit load list contains")
+
 # streamlit.text("Hello from Snowflake:")
 # streamlit.text(my_data_row)
 #streamlit.dataframe(my_data_row)
-streamlit.dataframe(my_data_rows)
+#streamlit.dataframe(my_data_rows)
+
 
 
 #New Section to display Fruityvice api response 
