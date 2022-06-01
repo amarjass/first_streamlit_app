4e5d4c6e4770c049fc251a4f92658beb12d59aa7
amarjass <105423167+amarjass@users.noreply.github.com>
2022-06-01 17:22:11 +0530

    Update streamlip_app.py

--- streamlip_app.py
+++ streamlip_app.py
@@ -88,32 +88,4 @@ try:
 #output it to the screen as a table
 #streamlit.dataframe(fruityvice_normalized)
 
-#dont run anything post here while  we troublshoot
-streamlit.stop()
 
-# import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("SELECT * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-#my_data_row = my_cur.fetchone()
-#streamlit.text("the fruit load list contains")
-streamlit.header("the fruit load list contains")
-# streamlit.text("Hello from Snowflake:")
-# streamlit.text(my_data_row)
-#streamlit.dataframe(my_data_row)
-streamlit.dataframe(my_data_rows)
-
-
-#New Section to display Fruityvice api response 
-#Second  text box
-streamlit.header('Fruityvice Fruit   2 Advice!')
-
-fruit_choice2 = streamlit.text_input('What fruit would you like information about?', 'apple')
-streamlit.write('The user entered ', fruit_choice2)
-# import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice2)
-streamlit.write('Thanks for adding ',fruit_choice2)
-
-my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
